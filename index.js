d0ec42a687a0bd938574f35a29f6438356eefce6
spkarthik2439 <spkarthik2439@gmail.com>
2024-01-18 16:07:54 +0530

    node test commit

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on the port ${port}`);
+});
